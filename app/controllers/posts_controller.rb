c038d998720af3c09f556eec4f3cca81caaee8dd
dobestan <dobestan@gmail.com>
2014-02-09 11:21:30 +0000

    error configured : post model migration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
